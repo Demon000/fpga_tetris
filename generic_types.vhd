e9b4f020838ed3dbd5e1a8164c75e2e46421cf78
Demon000 <demonsingur@gmail.com>
2019-08-27 13:59:53 +0300

    tetris: add lenght type

--- generic_types.vhd
+++ generic_types.vhd
@@ -2,10 +2,11 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 package graphics is
-    type point_2d is record
+    type length_2d is record
         x : integer;
         y : integer;
-    end record point_2d;
+    end record length_2d;
+    subtype point_2d is length_2d;
 
     constant point_2d_init : point_2d := (
         x => -1,
