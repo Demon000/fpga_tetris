4564186d4835e0d4b40e99a15a38631a25e25d83
Demon000 <demonsingur@gmail.com>
2019-08-28 14:50:52 +0300

    tetris: define constants for table size

--- tetris_types.vhd
+++ tetris_types.vhd
@@ -18,7 +18,14 @@ package tetris_types is
         z_color_id
     );
 
-    type tetris_table_data is array(0 to 21, 0 to 9) of piece_color_id;
+    constant table_width_blocks : integer := 10;
+    constant table_height_blocks : integer := 20;
+    constant table_virtual_height_blocks : integer := 22;
+
+    type tetris_table_data is array(
+        0 to table_virtual_height_blocks - 1,
+        0 to table_width_blocks - 1)
+    of piece_color_id;
 
     constant tetris_table_data_init : tetris_table_data := (others => (others => empty_color_id));
 end package tetris_types;
