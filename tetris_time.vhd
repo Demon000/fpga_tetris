d84376345041db50d685556a75cb87797f223297
Demon000 <demonsingur@gmail.com>
2019-08-30 12:37:45 +0300

    tetris: time: fix indentation

--- tetris_time.vhd
+++ tetris_time.vhd
@@ -16,8 +16,8 @@ package body tetris_time is
             level : tetris_level;
             tps : natural)
         return natural is
-     variable ticks : natural;
-     begin
+    variable ticks : natural;
+    begin
         case level is
             when 1 => ticks := tps * 1000 / 1000;
             when 2 => ticks := tps * 793 / 1000;
@@ -29,8 +29,8 @@ package body tetris_time is
             when 8 => ticks := tps * 134 / 1000;
             when 9 => ticks := tps * 93 / 1000;
             when 10 => ticks := tps * 64 / 1000;
-         end case;
+        end case;
 
-         return ticks;
-     end function get_fall_ticks_for_level;
+        return ticks;
+    end function get_fall_ticks_for_level;
 end package body tetris_time;
